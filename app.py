bea3c0e332c039edf1c25aa5365886b6c421e301
Matar7 <matar@linux.com>
2023-08-24 11:55:49 +0300

    fix html

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def index():
 
 # start the server with 'run()' method
 if __name__ == '__main__' :
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
